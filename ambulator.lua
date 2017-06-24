9ac9e60a5c6e2ae2a20cb664ec52c9a3493fd007
justinbarrick <justin.m.barrick@gmail.com>
2017-06-24 17:53:14 +0000

    Add test environment.

--- ambulator.lua
+++ ambulator.lua
@@ -1,6 +1,8 @@
 local robot = require("robot")
 
-local function left()
+local ambulator = {}
+
+function ambulator.left()
     print("Turn left.")
     robot.turnLeft()
     local success = robot.forward()
@@ -8,7 +10,7 @@ local function left()
     return success
 end
 
-local function right()
+function ambulator.right()
     print("Turn right.")
     robot.turnRight()
     local success = robot.forward()
@@ -16,12 +18,12 @@ local function right()
     return success
 end
 
-local function forward()
+function ambulator.forward()
     print("Forward")
     return robot.forward()
 end
 
-local function try_finding_forward(direction, backtrack)
+function ambulator.try_finding_forward(direction, backtrack)
     local steps = 0
 
     while true do
@@ -35,7 +37,7 @@ local function try_finding_forward(direction, backtrack)
         else
             steps = steps + 1
             
-            success = forward()
+            success = ambulator.forward()
             if success then
                 return steps
             end
@@ -43,18 +45,18 @@ local function try_finding_forward(direction, backtrack)
     end
 end
 
-local function go_forward()
-    local successful = forward()
+function ambulator.go_forward()
+    local successful = ambulator.forward()
 
     if successful then
         return true, 0
     end
 
-    local steps = try_finding_forward(left, right)
+    local steps = ambulator.try_finding_forward(ambulator.left, ambulator.right)
     if steps then
         return true, steps * -1
     else
-        steps = try_finding_forward(right, left)
+        steps = ambulator.try_finding_forward(ambulator.right, ambulator.left)
         if steps then
             return true, steps
         end
@@ -63,7 +65,7 @@ local function go_forward()
     return false
 end
 
-local function try(direction, steps)
+function ambulator.try(direction, steps)
     for  step=1,steps do
         if not direction() then
             return step - 1
@@ -73,7 +75,7 @@ local function try(direction, steps)
     return steps
 end
 
-local function move(x, y)
+function ambulator.move(x, y)
     local current_x = 0
     local current_y = 0
 
@@ -82,7 +84,7 @@ local function move(x, y)
             robot.turnAround()
         end
 
-        local success, x_steps = go_forward()
+        local success, x_steps = ambulator.go_forward()
         if not success then
             y_steps = 0
         elseif current_y > y then
@@ -98,12 +100,12 @@ local function move(x, y)
 
         local x_steps = nil
         if current_x > x then
-            x_steps = try(left, current_x - x)
+            x_steps = ambulator.try(left, current_x - x)
             if x_steps then
                 x_steps = x_steps * -1
             end
         elseif current_x < x then
-            x_steps = try(right, x - current_x)
+            x_steps = ambulator.try(right, x - current_x)
         end
 
         x_steps = x_steps or 0
@@ -114,8 +116,12 @@ local function move(x, y)
 end
 
 local args = {...}
-local x = tonumber(args[1]) or 0
-local y = tonumber(args[2]) or 0
+if args[1] ~= "ambulator" then
+    local x = tonumber(args[1]) or 0
+    local y = tonumber(args[2]) or 0
 
-print("moving to (" .. x .. "," .. y .. ")")
-move(x, y)
+    print("moving to (" .. x .. "," .. y .. ")")
+    ambulator.move(x, y)
+end
+
+return ambulator
